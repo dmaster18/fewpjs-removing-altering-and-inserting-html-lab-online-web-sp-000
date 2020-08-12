55efad6e0627dfc268710093c7571206d0e96879
David Strauch <dmaster18@gmail.com>
2020-08-12 20:53:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 let main = document.querySelector("main#main");
 main.remove();
 
-const newHeader = document.createElement('h1#victory')
-newHeader.textContent = "David is the champion"
-document.body.appendChild(newHeader)
+let newHeader = document.createElement('h1#victory')
+newHeader.innerHTML = "David is the champion"
+document.appendChild(newHeader)
